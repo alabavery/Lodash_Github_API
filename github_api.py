f198ceb139a88de646c4ea1a52e9b351e109bce3
Al Avery <averyab0@sewanee.edu>
2017-07-21 18:52:36 -0500

    Use repo ids instead of repo names

--- github_api.py
+++ github_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import time
-
+import socket
 
 # my wifi can sometimes cut off and back on, would hate to lose progress due to that
 def wait_until_wifi_is_connected():
@@ -9,6 +9,7 @@ def wait_until_wifi_is_connected():
 		try:
 			host = socket.gethostbyname('www.google.com')
 			s = socket.create_connection((host, 80), 2)
+			print("wifi is connected")
 			return
 		except:
 			time.sleep(30)
@@ -27,15 +28,16 @@ def get_json_response(url):
 	return json.loads(response.text)
 
 
-def get_organization_repo_names(base_url, organization_name):
+def get_organization_repo_ids(base_url, organization_name):
 	url = base_url + ('orgs/{0}/repos').format(organization_name)
 	response_data = get_json_response(url)
-	return [repo['name'] for repo in response_data]
+	return [repo['id'] for repo in response_data]
 
 
 def get_last_url_of_paginated(url):
 	response = requests.head(url)
 	last_link = response.links.get('last')
+	print('last page of current: ', last_link)
 
 	if not last_link: # response.links will be empty dict if only one page, so we manually return url of page 1
 		return url + '&page=1'
@@ -43,13 +45,14 @@ def get_last_url_of_paginated(url):
 
 
 # creates a generator that yields one page of results at a time
-def generate_repo_pull_requests(base_url, owner_name, repo_name):
-	url = base_url + ('repos/{0}/{1}/pulls?state=all').format(owner_name, repo_name)
+def generate_repo_pull_requests(base_url, repo_id):
+	url = base_url + ('repositories/{0}/pulls?state=all').format(repo_id)
 	last_url = get_last_url_of_paginated(url)
 	page_number = 1
 
 	while True:
-		page_url = base_url + ('repos/{0}/{1}/pulls?state=all&page={2}').format(owner_name, repo_name, page_number)
+		page_url = base_url + ('repositories/{0}/pulls?state=all&page={1}').format(repo_id, page_number)
+		print("working on ", page_url)
 		yield get_json_response(page_url)
 		page_number += 1
 
@@ -58,8 +61,8 @@ def generate_repo_pull_requests(base_url, owner_name, repo_name):
 
 
 # returns a list of ALL results
-def get_all_repo_pull_requests(base_url, owner_name, repo_name):
+def get_all_repo_pull_requests(base_url, repo_id):
 	all_pulls = []
-	for page_of_pulls in generate_repo_pull_requests(base_url, owner_name, repo_name):
+	for page_of_pulls in generate_repo_pull_requests(base_url, repo_id):
 		all_pulls.extend(page_of_pulls)
 	return all_pulls
--- index.py
+++ index.py
@@ -11,14 +11,14 @@ def save_as_json_file(file_path, data):
 
 
 BASE_URL = 'https://api.github.com/'
-lodash_repo_names = gapi.get_organization_repo_names(BASE_URL, 'lodash')
-print("Got repo names")
+#lodash_repo_ids = gapi.get_organization_repo_names(BASE_URL, 'lodash')
+lodash_repo_ids = ['57350423']
 base_path = os.path.dirname(os.path.realpath(__file__))
 
-for i, repo in enumerate(lodash_repo_names):
+for i, repo in enumerate(lodash_repo_ids):
 	file_path = os.path.join(base_path, repo) + '.json'
 	
 	if not os.path.isfile(file_path): # check if we've already saved this repo (this might not be the first time we've run program)
-		pulls = gapi.get_all_repo_pull_requests(BASE_URL, 'lodash', repo)
+		pulls = gapi.get_all_repo_pull_requests(BASE_URL, repo)
 		save_as_json_file(file_path, pulls)
 	print(str(i+1) + " repo(s) down...")
